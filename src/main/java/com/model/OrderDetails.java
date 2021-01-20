139ffdb105d96e2babbc0ab14ede8d6609d8dd71
arunsaradgi <76575824+arunsaradgi@users.noreply.github.com>
2021-01-20 10:51:13 +0530

    Initial commit

---
+++ src/main/java/com/model/OrderDetails.java
@@ -0,0 +1,72 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class OrderDetails {
+
+	@Id
+	@GeneratedValue
+	private int orderDetailsId;
+	private int quantity;
+	private double price;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	Orders orders; // only 1 object
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	Product product; // only 1 object
+
+	public Orders getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Orders orders) {
+		this.orders = orders;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getOrderDetailsId() {
+		return orderDetailsId;
+	}
+
+	public void setOrderDetailsId(int orderDetailsId) {
+		this.orderDetailsId = orderDetailsId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	// @Override
+	// public String toString() {
+	// return "OrderDetails [orderDetailsId=" + orderDetailsId + ", quantity=" +
+	// quantity + ", price=" + price + "]";
+	// }
+
+}
---
+++ src/test/java/com/ts/chooseWise/ChooseWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.chooseWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
